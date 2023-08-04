f8bc6b848de52903f69b52f0577f3b918e8718d9
Ramanuj Das <ramanujdas@Ramanujs-MacBook-Pro.local>
2023-08-04 10:04:25 +0530

    Form Validation

--- react-apps/online-shopping/src/components/product/AddProductPage.js
+++ react-apps/online-shopping/src/components/product/AddProductPage.js
@@ -4,6 +4,11 @@ const AddProductPage = ({productList, updateProductList}) => {
  
 
   let [product, updateProduct] = useState({});
+  let [validProductId, updateValidProductId] = useState(false);
+  let [validProductName, updateValidProductName] = useState(false);
+  let [validProductPrice, updateValidProductPrice] = useState(false);
+  let [validProductDescription, updateValidProductDescription] = useState(false);
+  let [validForm, updateValidForm] = useState(false);
 
   const addProduct = (e) => {
     e.preventDefault();
@@ -14,19 +19,47 @@ const AddProductPage = ({productList, updateProductList}) => {
 
     let newProductList = [...productList, product];
     updateProductList(newProductList);
-    resetForm();
     
   }
 
   const handleInputChange = (e) => {
     let inputName = e.target.name;
     let inputValue = e.target.value;
+    switch(inputName){
+      case 'productId':
+        updateValidProductId(inputValue > 0);
+        break;
+      case 'productName':
+        updateValidProductName(inputValue.length > 5);
+        break;
+      case 'productPrice':
+        updateValidProductPrice(inputValue > 0);
+        break;
+      case 'productDescription':
+        updateValidProductDescription(inputValue.length > 10);
+        break;
+    }
     updateProduct({...product, [inputName]:inputValue});
 
+    updateValidForm(validProductId && validProductName && validProductPrice && validProductDescription);
+
+    console.log(validProductId, validProductName, validProductPrice, validProductDescription);
+    console.log(validForm);
+
+   
+  
   }
+
+   
 
   const resetForm = () => {
     updateProduct({});
+    updateValidProductId(false);
+    updateValidProductName(false);
+    updateValidProductPrice(false);
+    updateValidProductDescription(false);
+    updateValidForm(false);
+    
   }
 
  
@@ -38,23 +71,26 @@ const AddProductPage = ({productList, updateProductList}) => {
           <label>Product Id</label>
           <input type='number' name='productId' className='form-control'  
             onChange={(e)=>{handleInputChange(e)}}/>
+            <p className='text-danger' hidden={validProductId}>Invalid ID</p>
           </div>
           <div className='form-group'>
           <label>Product Name</label>
           <input type='text' name='productName' className='form-control' 
             onChange={(e)=>{handleInputChange(e)}}/>
+            <p className='text-danger' hidden={validProductName}>Invalid Name</p>
           </div>
           <div className='form-group'>
           <label>Product Price</label>
           <input type='number' name='productPrice' className='form-control' onChange={(e)=>{handleInputChange(e)}}/>
+          <p className='text-danger' hidden={validProductPrice}>Invalid Price</p>
           </div>
           <div className='form-group'>
           <label>Product Description</label>
           <input type='text' name='productDescription' className='form-control' onChange={(e)=>{handleInputChange(e)}}/>
-
+          <p className='text-danger' hidden={validProductDescription}>Invalid Description</p>
           </div>
           <div className='form-group'>
-          <input type='submit' className='btn btn-primary' value='Add Product'/>
+          <input type='submit' disabled={!validForm} className='btn btn-primary' value='Add Product'/>
           <input type='reset' className='btn btn-dark' value='Reset'/>
           </div>
           </form>
